d4aa5bf5751470d15221983cdcc17974c74edb6c
Amanda <amanda.lrib@gmail.com>
2025-07-04 16:28:43 -0300

    feat: create tests

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
+using System.Text;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.OutputEncoding = Encoding.UTF8;
+
+Nokia celularAndroid = new Nokia(numero: "011 0000000", modelo: "N2", imei: "00000", memoria: 64);
+Iphone celularIOS = new Iphone(numero: "011 0000000", modelo: "N2", imei: "00000", memoria: 64);
+
+celularAndroid.InstalarAplicativo("Angry Birds");
+celularIOS.InstalarAplicativo("Plant vs Zombies");
+
+celularAndroid.Ligar();
+celularIOS.ReceberLigacao();
